6a2aebb7d567554e02ce28b2d7af47ed080bca7e
Imran Sayed <imranhsayed@gmail.com>
2019-11-17 20:35:38 +0530

    Use styled component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,16 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 const IndexPage = () => (
   <Layout>
@@ -14,6 +24,9 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+	  <Container>
+		  Styled Component
+	  </Container>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
