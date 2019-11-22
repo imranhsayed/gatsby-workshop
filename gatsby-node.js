e0a29c4c38534fbb0999705f084070daa7efa5c0
Imran Sayed <imranhsayed@gmail.com>
2019-11-22 17:29:17 +0530

    Update readme

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,30 +4,86 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-exports.createPages = ( { actions } ) => {
+const path = require( `path` );
 
+exports.createPages = async ( { actions, graphql } ) => {
+
+	const GET_POSTS      = `
+  query GET_POSTS($first:Int $after:String){
+    wpgraphql {
+      posts(
+        first: $first 
+        after:$after
+      ) {
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+        nodes {
+          id
+          uri
+          postId
+          title
+        }
+      }
+    }
+  }
+  `;
 	const { createPage } = actions;
+	const allPosts       = [];
+	const blogPages      = [];
+	let pageNumber       = 0;
+	const fetchPosts     = async variables =>
+		await graphql( GET_POSTS, variables ).then( ( { data } ) => {
+			const {
+				      wpgraphql: {
+					      posts: {
+						      nodes,
+						      pageInfo: { hasNextPage, endCursor },
+					      },
+				      },
+			      } = data
 
-	const employees = [
-		{
-			name: 'Imran',
-			job: 'WordPress Engineer'
-		},
-		{
-			name: 'John',
-			job: 'React Developer'
-		},
-	];
+			const nodeIds      = nodes.map( node => node.postId );
+			const blogTemplate = path.resolve( `./src/templates/blog.js` )
+			const blogPagePath = !variables.after ? `/` : `/page/${ pageNumber }`
 
-	/**
-	 * Loop through the data and provide the path, template, and context
-	 * (data that will be passed in the props’ pageContext) to createPage for each invocation
-	 */
-	employees.forEach( employee => {
-		createPage({
-			path: `/${ employee.name }`,
-			component: require.resolve( `./src/templates/employee-template.js` ),
-			context: { employee }
-		})
-	} );
+			blogPages[ pageNumber ] = {
+				path: blogPagePath,
+				component: blogTemplate,
+				context: {
+					ids: nodeIds,
+					pageNumber: pageNumber,
+					hasNextPage: hasNextPage,
+				},
+				ids: nodeIds,
+			};
+			nodes.map( post => {
+				allPosts.push( post )
+			} );
+			if ( hasNextPage ) {
+				pageNumber++;
+				return fetchPosts( { first: 12, after: endCursor } )
+			}
+			return allPosts
+		} );
+
+	await fetchPosts( { first: 12, after: null } ).then( allPosts => {
+		const postTemplate = path.resolve( `./src/templates/post.js` );
+
+		blogPages.map( blogPage => {
+			console.log( `createBlogPage ${ blogPage.context.pageNumber }` );
+			createPage( blogPage )
+		} );
+
+		allPosts.map( post => {
+			console.log( `create post: ${ post.uri }` );
+			createPage( {
+				path: `/blog/${ post.uri }/`,
+				component: postTemplate,
+				context: post,
+			} )
+		} )
+	} )
+
 };
